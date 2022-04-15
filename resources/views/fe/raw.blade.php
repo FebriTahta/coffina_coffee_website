149e26fa601a90c3841dd938d9491b49abedb0e4
FebriTahta <febrirtah@gmail.com>
2022-04-16 02:42:46 +0700

    bg color

--- resources/views/fe/raw.blade.php
+++ resources/views/fe/raw.blade.php
@@ -820,8 +820,8 @@
                 <div class="chef-items">
                     <!-- Single Item -->
                     @foreach ($team as $item)
-                    <div class="col-md-4 single-item" >
-                        <div class="item" style="width: 80%">
+                    <div class="col-md-4 single-item" style="text-align: center; align-content: center; align-items: center">
+                        <div class="item" style="width: 80%;text-align: center; align-content: center; align-items: center">
                             <div class="thumb">
                                 <img src="{{asset('img/team/'.$item->img)}}" alt="Thumb">
                             </div>
