5b48a21b7a48260fc5e2a19cce46b510df97464b
FebriTahta <febrirtah@gmail.com>
2022-04-26 09:41:20 +0700

    oke

--- resources/views/fe/raw.blade.php
+++ resources/views/fe/raw.blade.php
@@ -239,8 +239,8 @@
             color: #ffffff !important;
         }
 
-        /* nav.navbar.bootsnav {
-            background-color: #fff;
+        nav.navbar.bootsnav {
+            background-color: rgb(61, 31, 31);
             border-radius: 0;
             border: none;
             box-shadow: 0 8px 25px 0 rgba(0, 0, 0, 0.04);
@@ -248,7 +248,7 @@
             -webkit-box-shadow: 0 8px 25px 0 rgba(0, 0, 0, 0.04);
             -o-box-shadow: 0 8px 25px 0 rgba(0, 0, 0, 0.04);
             margin: 0;
-        } */
+        }
 
         nav.bg-dark.navbar.bootsnav {
             background-color: #1c1c1c;
@@ -989,7 +989,6 @@
             -webkit-box-shadow: 0 8px 25px 0 rgba(0, 0, 0, 0.04);
             -o-box-shadow: 0 8px 25px 0 rgba(0, 0, 0, 0.04);
             top: 0;
-            background-color: #0077b5;
         }
 
         nav.navbar.bootsnav.navbar-fixed.nav-box.no-background {
