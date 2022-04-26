0e5dbf49830f2654780d85dfbb4ffaa23c836de4
FebriTahta <febrirtah@gmail.com>
2022-04-26 09:47:17 +0700

    oke

--- resources/views/fe/raw.blade.php
+++ resources/views/fe/raw.blade.php
@@ -241,6 +241,7 @@
 
         nav.navbar.bootsnav {
             background-color: <?php echo $profile->warna_bg?>;
+            color : <?php echo $profile->warna_text?>;
             border-radius: 0;
             border: none;
             box-shadow: 0 8px 25px 0 rgba(0, 0, 0, 0.04);
